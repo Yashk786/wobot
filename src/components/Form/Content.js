b99250a3a8bcf0217addc21ae13575612a7479b3
Yash Kumar <87136660+Yashk786@users.noreply.github.com>
2023-01-12 19:37:11 +0530

    initil

--- src/components/Form/Content.js
+++ src/components/Form/Content.js
@@ -4,14 +4,14 @@ import From from "./From";
 import { Header } from "./Header";
 
 const styles = {
-  wrapper: "bg-gradient-to-r from-white via-[#e5e5e5] to-[#e5e5e5] h-fit flex flex-col w-full py-[1rem] px-[2rem] ",
-  logo__container:"mt-[1rem] ml-[2rem] cursor-pointer",
+  wrapper:
+    "bg-gradient-to-r from-white via-[#e5e5e5] to-[#e5e5e5] h-fit flex flex-col w-full py-[1rem] px-[2rem] ",
+  logo__container: "mt-[1rem] ml-[2rem] cursor-pointer",
   content__container: "flex flex-col relative",
   content:
     "  flex flex-col w-[500px] h-fit self-center content-center pt-[30px] pb-[36px]	px-[50px]   bg-[#ffffff]  rounded-[15px] shadow-[4px_4px_20px_rgba(157, 157, 157, 0.12)] 	",
-    div:"absolute w-[590px] h-[13.44px] left-[-124.11px] top-[440px] bg-[#E0E8FC] rotate-[-30deg]",
-    line:"absolute w-[488px] h-[13.44px] left-[890px] top-[430px] bg-[#3766E8] opacity-20 rotate-[-30deg]",
-
+  div: "absolute w-[800px] h-[13.44px] left-[-124.11px] top-[440px] bg-[#E0E8FC] rotate-[-30deg]",
+  line: "absolute w-[531px] h-[13.44px] left-[850px] top-[430px] bg-[#3766E8] opacity-20 rotate-[-30deg]",
 };
 
 const Content = () => {
@@ -19,9 +19,11 @@ const Content = () => {
     <div className={styles.wrapper}>
       {/* logo container */}
       <div className={styles.logo__container}>
-        <img className={styles.img} src="https://app.wobot.ai/assets/images/wobot-logo.png"></img>
+        <img
+          className={styles.img}
+          src="https://app.wobot.ai/assets/images/wobot-logo.png"
+        ></img>
         <div className={styles.div}></div>
-        <div className={styles.bg}></div>
         <div className={styles.line}></div>
       </div>
 
@@ -30,10 +32,8 @@ const Content = () => {
           <Header />
           <From />
         </div>
-        <Footer/>
-        </div>
-
-      
+        <Footer />
+      </div>
     </div>
   );
 };
